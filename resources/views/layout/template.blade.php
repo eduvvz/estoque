2e4f7a049cd67105a8ab7e5645f1c39a3e3069b4
Eduardo Cerqueira <eduardo@blanko.be>
2018-10-03 19:25:45 -0300

    Create connection in mysql, first CRUD is coming

---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link href="/css/app.css" rel="stylesheet">
+        <link rel="stylesheet" href="/css/custom.css">
+        <title>Controle de estoque</title>
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                  
+                <div class="navbar-header">      
+                    <a class="navbar-brand" href="/produtos">Estoque Laravel</a>
+                </div>
+                  
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/produtos">Listagem</a></li>
+                </ul>                 
+            </div>
+        </nav>
+        @yield('content')
+        <footer class="footer">
+            <p>© Livro de Laravel do Alura.</p>
+        </footer>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos', 'ProdutoController@lista');
+Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra');
